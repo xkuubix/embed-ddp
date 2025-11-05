659b4a99cceea16d9b303333ca46c7a5d80735dc
Jakub Buler <buler.jakub@gmail.com>
2025-11-05 21:24:49 +0100

    Remove commented-out lines

--- pytorch-ddp.sh
+++ pytorch-ddp.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=embed-ddp
-
 #SBATCH -p gpu-h100                           # Partition (queue) name
 #SBATCH -N 1                                  # Number of nodes (1 for GPU jobs)
 #SBATCH --ntasks-per-node=8                   # Number of CPU cores per node
 #SBATCH --mem=32000                           # Memory allocation (in MB)
 #SBATCH --time=71:59:59                       # Max execution time (hh:mm:ss)
 #SBATCH --output=/users/scratch1/jbuler/eucaim/outputs/%x_%j.out
-
-
-
-
-# export NCCL_DEBUG=INFO
-# export OMP_NUM_THREADS=4
-# torchrun --nproc_per_node=4 t.py
-
 
 GPU_NUMBER=4
 source /users/project1/pt01190/EUCAIM-PG-GUM/.venv_euc/bin/activate
