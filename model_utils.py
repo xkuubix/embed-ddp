fb9e615f6535a429a975b11bc35571486b1da71e
Jakub Buler <buler.jakub@gmail.com>
2025-12-01 14:08:52 +0100

    add EMA and add parameter in build_model to select backbone

--- model_utils.py
+++ model_utils.py
@@ -2,6 +2,10 @@ import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch import nn
 from models import GatedAttentionMIL
+from torchvision import models
+import torch
+import copy
+
 
 def deactivate_batchnorm(model):
     if isinstance(model, nn.BatchNorm2d):
@@ -9,17 +13,17 @@ def deactivate_batchnorm(model):
         model.running_mean = None
         model.running_var = None
 
-def build_model(device, is_ddp, rank, local_rank, use_pretrained=True):
+def build_model(device, is_ddp, rank, local_rank, use_pretrained=True, backbone='convnext_base'):
     """
     Builds model, handling pretrained-state broadcasting for DDP to avoid duplicated downloads.
     """
     if is_ddp and use_pretrained:
         if rank == 0:
-            m = GatedAttentionMIL()
+            m = GatedAttentionMIL(backbone=backbone, pretrained=use_pretrained)
             m.apply(deactivate_batchnorm)
             sd = m.state_dict()
         else:
-            m = GatedAttentionMIL()
+            m = GatedAttentionMIL(backbone=backbone, pretrained=use_pretrained)
             m.apply(deactivate_batchnorm)
             sd = None
 
@@ -29,12 +33,30 @@ def build_model(device, is_ddp, rank, local_rank, use_pretrained=True):
         sd = obj_list[0]
         m.load_state_dict(sd)
     else:
-        m = GatedAttentionMIL()
+        m = GatedAttentionMIL(backbone=backbone, pretrained=use_pretrained)
         m.apply(deactivate_batchnorm)
-
-    print(f"Model {m.__class__.__name__} built on device {device}")
-    
+    try:
+        print(f"Model {m.__class__.__name__} (with backbone {m.feature_extractor.__class__.__name__}) built on device {device}")
+    except:
+        print(f"Model {m.__class__.__name__} built on device {device}")
+ 
     m = m.to(device)
     if is_ddp:
         m = DDP(m, device_ids=[local_rank], output_device=local_rank)
     return m
+
+
+class ModelEMA:
+    def __init__(self, model, decay=0.999):
+        # make a copy of the model for EMA
+        self.ema = copy.deepcopy(model)
+        self.ema.eval()
+        self.decay = decay
+        for p in self.ema.parameters():
+            p.requires_grad_(False)
+
+    @torch.no_grad()
+    def update(self, model):
+        # update EMA weights after each training step
+        for ema_p, model_p in zip(self.ema.parameters(), model.parameters()):
+            ema_p.data.mul_(self.decay).add_(model_p.data, alpha=1-self.decay)
